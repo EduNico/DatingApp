8112ddc6cdf9bfd6a8518f5b58cc1b8648a1c775
EduNico <edunicocabral@gmail.com>
2023-05-19 01:36:43 -0400

    End of section 13

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -3,10 +3,10 @@ namespace API.DTOs
     public class UserDto  //this DTO  is use to Return a token whea a user logs in 
     {
         public string Username { get; set; } 
-
         public string Token { get; set; }
         public string PhotoUrl { get; set; }
         public string KnownAs  { get; set; }
+        public string Gender { get; set; }
 
     }
 }
